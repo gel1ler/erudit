7b89445ac0480bead1773d9a532b277a944dc953
Александр Абаничев <gel1ler@MacBook-Air-Aleksandr.local>
2024-08-06 16:12:04 +0300

    beta 0.7 fix

--- src/components/pages/index/groups/Carousel.tsx
+++ src/components/pages/index/groups/Carousel.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react'
-import { Box } from '@mui/material'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Card from './Card'
