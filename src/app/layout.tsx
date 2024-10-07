78aefe48de70594322bdf443995abce3d36eadbf
Aleksandr Abanichev <abanichev16@gmail.com>
2024-10-07 10:21:38 +0300

    Yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import Header from '@/components/layout/header/Header'
 import Footer from '@/components/layout/footer/Footer'
 import AOSProvider from '@/services/AOSProvider'
 import { Suspense } from 'react'
-import Loader from '../components/layout/loader'
 import Loading from '@/components/layout/Loading'
 import Script from 'next/script'
 
@@ -35,6 +34,31 @@ export default function RootLayout({
           </body>
         </AOSProvider>
       </ThemeRegistry>
+      {/* Яндекс.Метрика */}
+      <Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(98564600, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });
+          `,
+        }}
+      />
+      <noscript>
+        <div>
+          <img src="https://mc.yandex.ru/watch/98564600" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+        </div>
+      </noscript>
     </html>
   )
 }
