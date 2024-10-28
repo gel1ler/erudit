e3ab52c6e54c7e6db70f5bc13e528acb853bea19
Aleksandr Abanichev <abanichev16@gmail.com>
2024-10-28 09:07:12 +0300

    Overflow fixed

--- src/components/pages/ECenter/pageLayout.tsx
+++ src/components/pages/ECenter/pageLayout.tsx
@@ -11,29 +11,31 @@ export default function PageLayout({ id, children }: { id: number, children: Rea
     return (
         <>
             <Loader />
-            <Box className='relative flex flex-col gap-4 my-40 max-w-3xl mx-auto overflow-hidden'>
-                <div className="absolute -z-10 opacity-35 w-full h-full top-0 left-0 transition-transform duration-500 translateToTop">
-                    {item.icons.map((icon, key) =>
-                        <Image
-                            key={key}
-                            src={icon.src}
-                            alt='Иконка эрудит центр'
-                            width={icon.size}
-                            height={icon.size}
-                            className={`absolute ${icon.noMd ? 'hidden lg:block' : ''}`}
-                            style={{
-                                top: icon.position[0], right: icon.position[1], bottom: icon.position[2], left: icon.position[3],
-                                transform: `rotate(${icon.rotate}deg)`,
-                                opacity: icon.opacity ? icon.opacity : 'unset'
-                            }}
-                        />
-                    )}
-                </div>
-                <Box className='flex flex-col gap-6 items-center my-32'>
-                    <Title>{item.title}</Title>
-                    <Typography textAlign='center' className='w-2/3'>
-                        {item.description}
-                    </Typography>
+            <Box className='w-screen overflow-hidden'>
+                <Box className='relative flex flex-col gap-4 my-40 max-w-3xl mx-auto'>
+                    <div className="absolute -z-10 opacity-35 w-full h-full top-0 left-0 transition-transform duration-500 translateToTop">
+                        {item.icons.map((icon, key) =>
+                            <Image
+                                key={key}
+                                src={icon.src}
+                                alt='Иконка эрудит центр'
+                                width={icon.size}
+                                height={icon.size}
+                                className={`absolute ${icon.noMd ? 'hidden lg:block' : ''}`}
+                                style={{
+                                    top: icon.position[0], right: icon.position[1], bottom: icon.position[2], left: icon.position[3],
+                                    transform: `rotate(${icon.rotate}deg)`,
+                                    opacity: icon.opacity ? icon.opacity : 'unset'
+                                }}
+                            />
+                        )}
+                    </div>
+                    <Box className='flex flex-col gap-6 items-center my-32'>
+                        <Title>{item.title}</Title>
+                        <Typography textAlign='center' className='w-2/3'>
+                            {item.description}
+                        </Typography>
+                    </Box>
                 </Box>
             </Box>
             <Box className='mb-20 flex flex-col items-center gap-16 px-4'>
